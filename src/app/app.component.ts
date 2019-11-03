f2ecb72d73cf16b67cd5a5d731172ba0c0ef7c08
davidmottet <contact@davidmottet.com>
2019-11-03 13:50:13 +0100

    [upd]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'body',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'davidmottet-angular';
+	title = 'davidmottet-angular';
 }
